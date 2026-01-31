f6753663d0bd1c52934d2b7ded63483e5b319f4e
Chethan-044 <chetuc0404@gmail.com>
2026-01-31 16:19:39 +0530

    redy for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,25 @@ import express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import messageRoute from "./routes/message.route.js"
+import path from "path"
+
+
 
 dotenv.config();
-const app = express();
 
+const app = express();
+const __dirname = path.resolve()
 
 app.use("/api/auth",authRoutes)
 app.use("/api/message" , messageRoute)
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*",(_,res)=>{
+        res.send(path.join(__dirname, "../frontend" , "dist" , "index.html"))
+    })
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen( PORT,()=>{
