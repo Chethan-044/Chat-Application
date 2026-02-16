112ac1910ac493e377b5e3647aff121968f6ec4f
Chethan-044 <chetuc0404@gmail.com>
2026-02-16 22:52:54 +0530

    rdy for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
-import messageRoute from "./routes/message.route.js"
 import path from "path"
 import connectDB from "./lib/db.js"
+
+
+import authRoutes from "./routes/auth.route.js"
+import messageRoute from "./routes/message.route.js"
 
 
 dotenv.config();
@@ -18,7 +20,7 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
     app.get("*",(_,res)=>{
-        res.send(path.join(__dirname, "../frontend" , "dist" , "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend" , "dist" , "index.html"))
     })
 }
 
