f33c310b820e2e19adb10d7e9b22f2fcdcd48c93
Chethan-044 <chetuc0404@gmail.com>
2026-02-20 17:45:51 +0530

    fixed login, added rate limiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,9 +2,10 @@ import express from 'express'
 import { signup,login,logout,updateProfile } from '../controllers/auth.controller.js'
 import { protectedRoute } from '../middlewares/auth.middleware.js'
 import upload from "../config/multer.js";
-
+import { arcjetProtection } from '../middlewares/arcjet.middleware.js';
 
 const router = express.Router();
+router.use(arcjetProtection)
 
 router.post("/signup",signup)
 router.post("/login",login)
@@ -13,4 +14,7 @@ router.post("/logout",logout)
 
 router.put("/update-profile",protectedRoute , upload.single("profilePic"), updateProfile)
 
+router.get("/check",protectedRoute,(req,res)=>{
+    res.status(200).json({message:"You are authenticated",user:req.user})
+})
 export default router;
